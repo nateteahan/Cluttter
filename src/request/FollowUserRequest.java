6764d6a6264f3026c755badb941ec51605c7503e
NatetheWizard <nateteahan@gmail.com>
2019-11-20 11:22:28 -0700

    getUser working

--- src/request/FollowUserRequest.java
+++ src/request/FollowUserRequest.java
@@ -1,10 +1,8 @@
 package request;
 
 public class FollowUserRequest {
-    public String followerHandle;           //Do I need this to get the handle of the followee from the URL?
+    public String followerHandle;
     public String followeeHandle;
-//    public String followerName;
-//    public String followeeName;
 
     public String getFollowerHandle() {
         return followerHandle;
@@ -21,20 +19,4 @@ public class FollowUserRequest {
     public void setFolloweeHandle(String followeeHandle) {
         this.followeeHandle = followeeHandle;
     }
-
-//    public String getFollowerName() {
-//        return followerName;
-//    }
-//
-//    public void setFollowerName(String followerName) {
-//        this.followerName = followerName;
-//    }
-//
-//    public String getFolloweeName() {
-//        return followeeName;
-//    }
-//
-//    public void setFolloweeName(String followeeName) {
-//        this.followeeName = followeeName;
-//    }
 }
